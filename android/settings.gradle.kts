66540023822940f378ad871c9fcedae617396381
dsc1215 <dsc1215@users.noreply.github.com>
2026-07-09 02:07:22 +0800

    fix release workflow builds

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,22 +1,35 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.aliyun.com/repository/public")
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+        val useOfficialRepos = providers.environmentVariable("CI_MAVEN_REPOS").orNull == "official"
+        if (useOfficialRepos) {
+            google()
+            mavenCentral()
+            gradlePluginPortal()
+        } else {
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/gradle-plugin")
+            maven("https://maven.aliyun.com/repository/public")
+            google()
+            mavenCentral()
+            gradlePluginPortal()
+        }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/central")
-        google()
-        mavenCentral()
+        val useOfficialRepos = providers.environmentVariable("CI_MAVEN_REPOS").orNull == "official"
+        if (useOfficialRepos) {
+            google()
+            mavenCentral()
+        } else {
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/public")
+            maven("https://maven.aliyun.com/repository/central")
+            google()
+            mavenCentral()
+        }
     }
 }
 
