29db6b049e72af6a464ef8b3bea2d98ab90f9d7a
dsc1215 <dsc1215@users.noreply.github.com>
2026-07-10 02:48:49 +0800

    add linux man page

--- packaging/linux/build_deb.sh
+++ packaging/linux/build_deb.sh
@@ -11,6 +11,7 @@ mkdir -p \
   "$DEB_ROOT/DEBIAN" \
   "$DEB_ROOT/usr/bin" \
   "$DEB_ROOT/usr/share/applications" \
+  "$DEB_ROOT/usr/share/man/man1" \
   "$DEB_ROOT/usr/share/m3u8-downloader" \
   "$OUT_DIR"
 
@@ -30,6 +31,7 @@ CONTROL
 cp -a "$ROOT/m3u8_downloader" "$DEB_ROOT/usr/share/m3u8-downloader/"
 cp "$ROOT/README.md" "$ROOT/setup.py" "$ROOT/requirements.txt" "$ROOT/requirements-desktop.txt" "$DEB_ROOT/usr/share/m3u8-downloader/"
 cp "$ROOT/packaging/linux/m3u8-downloader.desktop" "$DEB_ROOT/usr/share/applications/"
+gzip -9c "$ROOT/packaging/linux/m3u8-downloader.1" > "$DEB_ROOT/usr/share/man/man1/m3u8-downloader.1.gz"
 
 cat > "$DEB_ROOT/usr/bin/m3u8-downloader" <<'SH'
 #!/bin/sh
